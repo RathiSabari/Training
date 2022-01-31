c9471a5bd5332cdc6b51d6218ac07b704946087e
Rathi <rathipriya16@gmail.com>
2022-01-31 18:29:54 +0800

    Training proj implementing angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule,ReactiveFormsModule,RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { compileDeclareClassMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl, FormArray} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'borzo';
-}
+  //title = 'borzo';
+   courierform = new FormGroup({
+    pickupaddress: new FormControl(''),
+    deliveryaddress: new FormControl(''),
+  });
+  pickupadd='';
+  deliveryadd='';
+  show:boolean=false;
+  bookcouriershow:boolean=true;
+  msgsshow:boolean=false;
+  msg='';
+  disbutton=true;
+  
+  pdform=new FormGroup({
+    fromname: new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z ]*$')]),
+    fromphonenumber: new FormControl('',[Validators.required,Validators.pattern('^[0-9]{10}$')]),
+    toname: new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z ]*$')]),
+    tophonenumber: new FormControl('',[Validators.required,Validators.pattern('^[0-9]{10}$')]),
+    courieritem: new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z ]*$')]),
+    weight: new FormControl('',[Validators.required,Validators.pattern('^[0-9]{1,2}$')]),
+  });
+
+ senttoPDpage()
+  {
+    this.pickupadd=this.courierform.controls['pickupaddress'].value;
+  this.deliveryadd =this.courierform.controls['deliveryaddress'].value;
+    if(this.pickupadd && this.deliveryadd)
+    {
+     this.show=true;
+     this.bookcouriershow=false;
+     this.msgsshow=false;
+    }
+    else{
+      this.msgsshow=true;
+      this.msg="Please enter pick-up and delivery address";
+    }
+  }
+  makepayment()
+    {
+     
+      if(this.pdform.valid)
+      {
+        
+        alert("Thank you for booking with us "+this.pdform.controls['fromname'].value + " You will receive your booking id via sms")
+        this.pdform.reset();
+        this.show=false;
+        this.bookcouriershow=true;
+        this.courierform.reset();
+        
+      }
+      
+      else{
+      alert("Please fill the form with valid values")
+      }
+      
+     
+    }
+    isDisabled()
+  {
+    if(this.pdform.valid)
+    {
+      this.disbutton=false;
+     
+      
+    }
+    
+    return this.disbutton;
+  }   
+  }
